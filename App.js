c33899c4ef8e81dda93c5e90de3322e5df85d588
alx-askw <alexw.askew77@gmail.com>
2023-08-02 17:24:14 +0100

    change refactor happening

--- App.js
+++ App.js
@@ -9,6 +9,9 @@ app.use(express.urlencoded({ extended: true })); //https://stackoverflow.com/que
 const indexRouter = require('./routes/index.js');
 const gameRouter = require('./routes/game.js');
 
+// TODO: Add turn route
+// TODO: consider added ai turn vs 2player turn, one automatically does its turn whilst the other is just another player turn
+// TODO: Alternatively consider refactoring and having the program just spit out a random attack if ai, No computerplayer class needed
 app.use('/', indexRouter);
 app.use('/game', gameRouter);
 
